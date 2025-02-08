f085a3ef16ca80c079454b8fab1b0eb1f3e7f850
Inshamhaque <haqueinsham@gmail.com>
2025-02-08 19:06:34 +0530

    some ref

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,18 +1,12 @@
 export const Quote = () => {
-    return (
-        <div className="flex justify-center bg-white shadow-lg rounded-t-lg p-4 text-center">
-            <div className="max-w-lg">
-                <div className="text-l font-bold">
-                    "Unleash your voice, share your story, and inspire the world—<br></br>join our blogging community today!!"
-                </div>
-                <div className="mt-4">
-                    Inshamul Haque,
-                </div>
-                <div className="text-slate-500">
-                    CEO
-                </div>
-            </div>
-            
+  return (
+    <div className="flex justify-center bg-white shadow-lg rounded-t-lg p-4 text-center">
+      <div className="max-w-lg">
+        <div className="text-l font-bold">
+          "Unleash your voice, share your story, and inspire the world—<br></br>
+          join our blogging community today!!"
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
